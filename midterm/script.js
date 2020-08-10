dc352b7e3a5b12a801efc18fb5d9cc9e71737eb7
navpreet1999 <57304236+navpreet1999@users.noreply.github.com>
2020-08-09 19:21:33 -0700

    Add files via upload

---
+++ midterm/script.js
@@ -0,0 +1,27 @@
+//var x =4/3;
+//var radius = Number(prompt("enter your radius:"));
+//var multuiplication =x * PIE * radius *radius *radius;
+//alert("your sphere is :"+ multiplication);
+
+//var radius = Number(prompt("enter your age:"));  // STEP 1
+
+//STEP 2
+//if(age>= 1 && age <=19){
+	//alert('You are not a major yet.')
+//}
+
+//STEP 3
+//if(age>= 20 && age <=45){
+	//alert('You are a major, but not a senior yet.')
+//}
+
+STEP 4
+//if(age>= 46 && age <=90){
+	//alert('You are senior, but not a yet retired.')
+//}
+var i= 1;
+while(i <= 25)P{    // start  ->1 and i is smaller than or equal to 25
+   alert("Bonjour");
+   i = i + 1;
+   }
+   
